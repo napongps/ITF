20c5289143679d41910488c78bc512d348afe2e2
napongps <73694270+napongps@users.noreply.github.com>
2020-11-13 17:35:54 +0700

    Update show.php

--- show.php
+++ show.php
@@ -18,12 +18,14 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM GuestBook');
 ?>
-<table width="600" border="1" class="thead-dark">
+<table width="600" border="1">
+     <thead class="thead-dark">
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Link </div></th>
   </tr>
+           </thead>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
