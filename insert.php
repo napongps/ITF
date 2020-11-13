20fda60060d5d2ee7c3d69614e7613350b899d0f
napongps <73694270+napongps@users.noreply.github.com>
2020-11-13 17:17:03 +0700

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'napongps.mysql.database.azure.com', 'napongps@napongps', 'Jklhgdf1123', 'ITFlab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$Name = $_POST['Name'];
+$Comment = $_POST['Comment'];
+$Link = $_POST['Link'];
+
+
+$sql = "INSERT INTO GuestBook (Name , Comment , Link) VALUES ('$Name', '$Comment', '$Link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
