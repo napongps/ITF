777de787b5a1f2e4a9fbf00b7e3af4c3f3ac42fa
napongps <73694270+napongps@users.noreply.github.com>
2020-11-13 18:17:27 +0700

    Update show.php

--- show.php
+++ show.php
@@ -13,8 +13,8 @@
 <body>
 <center>
 <?php
-function delte() {
-  $sql ="DELETE FROM GuestBook WHERE id= $Result['Name']";
+function delte(idaz) {
+  echo "Hooooo"
 }
 <?php
 $conn = mysqli_init();
@@ -41,11 +41,10 @@ while($Result = mysqli_fetch_array($res))
 ?>
 <tbody>
   <tr>
-    <td><?php echo $Result['ID'];?></div></td>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
-  <td><button oncilk=date>ลบ</button><button oncilk="">แก้ไข</button></td>
+  <td><button oncilk="">ลบ</button><button oncilk="">แก้ไข</button></td>
   </tr>
  </tbody>
 <?php
