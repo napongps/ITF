ca0ac3905f297c382b24e0fcb572e99d3963784a
napongps <73694270+napongps@users.noreply.github.com>
2020-11-14 17:50:07 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,8 +1,27 @@
 <?php
+
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'napongps.mysql.database.azure.com', 'napongps@napongps', 'Jklhgdf1123', 'ITFlab', 3306);
-$id=$_REQUEST['ID'];
-$query = "DELETE FROM GuestBook WHERE ID=$id"; 
-$result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: show.php"); 
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+$id = $_POST['id']
+
+
+$sql = "DELETE FROM GuestBook WHERE ID='$id'";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
 ?>
