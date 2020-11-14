106817743a7a201246dcb2b8df99d58d8daa6f15
napongps <73694270+napongps@users.noreply.github.com>
2020-11-14 17:16:38 +0700

    Update show.php

--- show.php
+++ show.php
@@ -27,7 +27,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
-    <td><a href="#" class="btn btn-primary">ลบ</a></td>
+    <td><a href="https://napong.azurewebsites.net/delete.php" class="btn btn-primary">ลบ</a></td>
     <td><a href="#" class="btn btn-primary">แก้ไข</a></td>
 
   </tr>
