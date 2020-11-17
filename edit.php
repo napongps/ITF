487caf94928fceb570d78d68a497842d70f87459
napongps <73694270+napongps@users.noreply.github.com>
2020-11-17 22:54:29 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -4,16 +4,14 @@
   $id = $_GET['id'];
   $result = mysqli_query($conn,"SELECT * FROM GuestBook WHERE id='$id'");
   $row = mysqli_fetch_assoc($result);
-  $name = $row['Name'];
-  $comment = $row['Comment'];
-  $link = $row['Link'];
-  if(isset($_POST['submit'])){
-    $id = $_GET['id'];
-    $name2 = $POST['name'];
-    $comment2 = $POST['comment'];
-    $link2 = $POST['link'];
 
-    $sql = "UPDATE GuestBook SET Name='$name2',Comment='$comment2', Link='$link2' WHERE id='id'";
+  if(isset($_REQUEST['submit'])){
+    $id = $_GET['id'];
+    $name = $POST['name'];
+    $comment = $POST['comment'];
+    $link = $POST['link'];
+
+    $sql = "UPDATE GuestBook SET Name='$name',Comment='$comment', Link='$link' WHERE id='id'";
 
     if(mysqli_query($conn,$sql)){
       echo"The record has been update";
@@ -31,11 +29,11 @@
 <body>
   <!-- <form action = "insert.php" method = "post" id="CommentForm" > -->
     Name:<br>
-    <input type="text" name = "name" id="idName" value="<?= $name; ?>"> <br>
+    <input type="text" name = "name" id="idName" value="<?= $row['name']; ?>"> <br>
     Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" value="<?= $comment; ?>"></textarea><br>  
+    <textarea rows="10" cols="20" name = "comment" id="idComment" value="<?= $row['comment']; ?>"></textarea><br>  
     Link:<br>
-    <input type="text" name = "link" id="idLink" value="<?= $link; ?>"> <br><br>
+    <input type="text" name = "link" id="idLink" value="<?= $row['link']; ?>"> <br><br>
     <input type="submit" name = "submit" id="commentBtn">
   </form>
 </body>
