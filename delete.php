55ea25109ce70c27e2339dd126c8e66b5216e671
napongps <73694270+napongps@users.noreply.github.com>
2020-11-17 17:13:49 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
 
 $conn = mysqli_init();
 $conn = mysqli_real_connect($conn, 'napongps.mysql.database.azure.com', 'napongps@napongps', 'Jklhgdf1123', 'ITFlab', 3306);
-$id = $_REQUEST['delete'];
+$id = $_GET['delete'];
 
 $sql="DELETE FROM GuestBook WHERE id=$id";
 if(mysqli_query($conn,$sql)){
