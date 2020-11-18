7c35aabe78369e3482305341f6cb063ad2a1fff2
napongps <73694270+napongps@users.noreply.github.com>
2020-11-18 20:59:09 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -15,6 +15,7 @@ $sql =  "DELETE FROM GuestBook WHERE id='$id'";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record deleted successfully";
+    header("location:show.php");
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
