f483c21b991b780070c0248159326ec11c6fe578
napongps <73694270+napongps@users.noreply.github.com>
2020-11-18 20:52:37 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@ $conn = mysqli_init();
 $conn = mysqli_real_connect($conn, 'napongps.mysql.database.azure.com', 'napongps@napongps', 'Jklhgdf1123', 'ITFlab', 3306);
 
 $id = $_REQUEST['id'];
-$sql = "DELETE FROM GuestBook WHERE id='$id'";
+$sql = "DELETE FROM guestbook WHERE id='$id'";
 if(mysqli_query($conn, $sql)){
   echo"The record has been deleted";
   header("location:show.php");
