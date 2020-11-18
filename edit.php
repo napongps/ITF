b2c13128d04a42150508cac9c811316491770523
napongps <73694270+napongps@users.noreply.github.com>
2020-11-18 21:39:22 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -8,6 +8,18 @@ if (mysqli_connect_errno($conn))
 $id = $_GET['id'];
 $result = mysqli_query($conn, "SELECT * FROM GuestBook WHERE id='$id'");
 $row=mysqli_fetch_assoc($result);
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $comment = $_POST['comment'];
+    $link = $_POST['link'];
+
+    $sql = "UPDATE GuestBook SET Name='$name', Comment='$comment', Link='$link' WHERE id='$id'";
+
+    if(mysqli_query($conn, $sql)){
+        echo "Hooray"
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,7 +34,7 @@ $row=mysqli_fetch_assoc($result);
     <textarea rows="10" cols="20" name = "comment" id="idComment" value="<?  $row['Comment']; ?>"></textarea><br>  
     Link:<br>
     <input type="text" name = "link" id="idLink" value="<?= $row['Link']; ?>"> <br><br>
-    <input type="submit" id="commentBtn" value="update">
+    <input type="submit" name="submit" id="commentBtn" value="update">
   </form>
 </body>
 </html>
